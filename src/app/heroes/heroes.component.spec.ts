975d2119dca52ec04b41d56dd1278f2e4e900e6f
Sofi43 <201810557@tese.edu.mx>
2021-03-26 00:39:42 -0600

    Cambio paraagregar los supeheroes en lista

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,3 +1,5 @@
+
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroesComponent } from './heroes.component';
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import{Hero}from "./hero";
+
+export const HEROES:Hero[]=[
+ {id:11, name:"Superman", edad:30, SuperPoder:"Curacion rapida", Identidad:"Clark Josepth", Universo:"DC Comics "},
+ {id:12, name:"Batman", edad:30,SuperPoder:"Ser multimillonario", Identidad: "Bruce Wayne", Universo:"DC Comics"},
+ {id:13, name:"Hulk", edad:40,SuperPoder:"Super fuerza", Identidad:"Robert Bruce", Universo:"Marvel"},
+ {id:14, name:"Ironman", edad:25, SuperPoder:"Vuelo supersonico", Identidad:"Tony Stark", Universo:"Marvel"},
+ {id:15, name:"Thor", edad:1000, SuperPoder:"Fuerza sobrehumana", Identidad:"Thor Odison", Universo:"Marvel"},
+ {id:16, name:"Spiderman", edad:20, SuperPoder:"Agilidad", Identidad:"Peter Parker", Universo:"Marvel"},
+ {id:17, name:"Black Widow", edad:28, SuperPoder:"Resistencia", Identidad:"Natalia Aliavovna", Universo:"Marvel"},
+ {id:18, name:"Flash", edad:28, SuperPoder:"Velocidad", Identidad:"Jay Garrick", Universo:"DC Comics"},
+ {id:19, name:"Bruja Escarlata", edad:36, SuperPoder:"Poderes psiquicos", Identidad:"Wanda Maximoff", Universo:"Marvel"},
+ {id:20, name:"Wonder Woman", edad: 30, SuperPoder:"Reflejos e inmortalidad", Identidad:"Princesa Diana", Universo:"DC Comics"},
+
+]
